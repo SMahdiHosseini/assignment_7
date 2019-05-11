0157d844f5980d2ba33d84a743617c5130545645
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2019-05-11 23:13:10 +0430

    correction of constructure

---
+++ publisher.cpp
@@ -0,0 +1,9 @@
+#include "publisher.h"
+
+#define DEFAULT_CASH 0
+
+using namespace std;
+
+Publisher::Publisher(int _id, std::string _email, std::string _username, std::string _password, int _age, bool _publisher)
+ : User(_id, _email, _username, _password, _age, _publisher) {}
+
--- publisher.h
+++ publisher.h
@@ -7,7 +7,8 @@
 class Publisher : public User
 {
 public:
-    Publisher(int _id, std::string _email, std::string _username, std::string _password, int _age, bool _publisher);
+    Publisher(int _id, std::string _email, std::string _username, std::string _password, int _age, bool _publisher) 
+    : User(_id, _email, _username, _password, _age, _publisher);
     virtual void add_film(Film* new_film);
     virtual void edit_film(int film_id, Film* edited_film);
     virtual void delete_film(int film_id);
