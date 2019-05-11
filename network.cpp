d7e0151d2089d8c106f811f0cd1b38cf09140a14
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2019-05-12 00:21:00 +0430

    signup functions

--- network.cpp
+++ network.cpp
@@ -1,6 +1,13 @@
 #include "network.h"
 
+#define DEFAULT_CASH 0
+
 using namespace std;
+
+Network::Network()
+{
+    cash = DEFAULT_CASH;
+}
 
 User* Network::find_logged_in_user()
 {
@@ -9,7 +16,17 @@ User* Network::find_logged_in_user()
             return users[i];
 }
 
-void Network::signup(string email, string username, string password, int age, bool publisher)
+void Network::signup(int id, string email, string username, string password, int age, bool publisher)
 {
-    
+    if(publisher)        
+        add_publisher(id, email, username, password, age, publisher);
+    User* new_user = new User(id,email, username, password, age, publisher);
+    users.push_back(new_user);
 }
+
+void Network::add_publisher(int id, std::string email, std::string username, std::string password, int age, bool publisher)
+{
+    Publisher* new_publisher = new Publisher(id, email, username, password, age, publisher);
+    users.push_back(new_publisher);
+}
+
--- network.h
+++ network.h
@@ -2,13 +2,16 @@
 #define NETWORK_H
 
 #include <vector>
+#include "user.h"
 #include "publisher.h"
 #include "film.h"
 
 class Network
 {
 public:
-    void signup(std::string email, std::string username, std::string password, int age, bool publisher);
+    Network();  
+    void signup(int id, std::string email, std::string username, std::string password, int age, bool publisher);
+    void add_publisher(int id, std::string email, std::string username, std::string password, int age, bool publisher);
     void login(std::string username, std::string pasword);
     User* find_logged_in_user();
 private:
