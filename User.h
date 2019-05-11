4233e5087c84ddb08ff97e650bf9c435772190b9
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2019-05-11 10:27:55 +0430

    change "cash" data format and add getter functions

--- User.h
+++ User.h
@@ -14,6 +14,11 @@ class User
 {
 public:
 	User();
+	string get_username() {return username;}
+	string get_password() {return password;}
+	int get_id() {return id;}
+	bool check_login() {return login;}
+	bool check_publsher() {return publisher;}
 	void give_notification(string notification); 
 	void show_unread_notifications();
 	void show_notifications();
@@ -21,8 +26,8 @@ public:
 	void folow_publisher(int publisher_id);
 	void increas_money(double amount);
 	void search_film(Film* film);
-	void buy_film(int film_id);
-	void rate_film(int film_id, double score);
+	void buy_film(Film* new_film);
+	void rate_film(Film* film, double score);
 	void comment_on_film(int film_id, string content);
 protected:
 	string username;
@@ -31,7 +36,7 @@ protected:
 	int id;
 	bool login;
 	bool publisher;
-	double cash;
+	int cash;
 	vector<Film*> bought_films;
 	vector<Notification> notifications; 
 	vector<Publisher*> following_publishers;
