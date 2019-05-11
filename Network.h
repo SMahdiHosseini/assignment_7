e39ced5f216d8dea3f4c8a1bd4fd0e73bf910905
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2019-05-11 12:42:30 +0430

    Add files via upload

--- Network.h
+++ Network.h
@@ -10,14 +10,14 @@ using namespace std;
 class Network
 {
 public:
-    void signup();
-    void login();
+    void signup(string email, string username, string password, int age, bool publisher);
+    void login(string username, string pasword);
     User* find_logged_in_user();
     
 private:
     vector<User*> users;
     vector<Film*> films;
-    double cash;
+    int cash;
 };
 
 #endif
