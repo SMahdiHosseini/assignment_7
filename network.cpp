927022ddf1261ad63d8bb0692486a5d09b7cfffd
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2019-05-12 00:42:16 +0430

    add functions for login

--- network.cpp
+++ network.cpp
@@ -1,4 +1,5 @@
 #include "network.h"
+#include "exceptions.h"
 
 #define DEFAULT_CASH 0
 
@@ -16,8 +17,18 @@ User* Network::find_logged_in_user()
             return users[i];
 }
 
+bool Network::check_existed_user(string username)
+{
+    for (int i = 0; i < users.size(); i++)
+        if(users[i]->get_username() == username)
+            return true;
+    return false;
+}
+
 void Network::signup(int id, string email, string username, string password, int age, bool publisher)
 {
+    if(check_existed_user(username))
+        throw BadRequest();
     find_logged_in_user()->logout();
     if(publisher)        
         add_publisher(id, email, username, password, age, publisher);
@@ -31,3 +42,16 @@ void Network::add_publisher(int id, std::string email, std::string username, std
     users.push_back(new_publisher);
 }
 
+User* Network::find_user(string username, string password)
+{
+    for (int i = 0; i < users.size(); i++)
+        if(users[i]->get_username() == username && users[i]->get_password() == password)
+            return users[i];
+    throw BadRequest();
+}
+
+void Network::login(string username, string password)
+{
+    find_logged_in_user()->logout();
+    find_user(username, password)->login_user();
+}
--- network.h
+++ network.h
@@ -13,6 +13,8 @@ public:
     void signup(int id, std::string email, std::string username, std::string password, int age, bool publisher);
     void add_publisher(int id, std::string email, std::string username, std::string password, int age, bool publisher);
     void login(std::string username, std::string pasword);
+    bool check_existed_user(string username);
+    User* find_user(std::string username, std::string password);
     User* find_logged_in_user();
 private:
     std::vector<User*> users;
