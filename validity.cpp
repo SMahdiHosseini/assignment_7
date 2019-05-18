c38f87c069b8faa3464459a7cd0e1f86d5bf2283
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2019-05-18 10:33:14 +0430

    write check_integer and check_email function body

--- validity.cpp
+++ validity.cpp
@@ -6,3 +6,22 @@ bool Validity::signup_validity(string age, string email, string publisher)
 {
     return check_integer(age) && check_email(email) && check_publisher(publisher);
 }
+
+bool Validity::check_integer(string integer)
+{
+    for (int i = 0; i < integer.size(); i++)
+        if(integer[i] > '9' || integer[i] < '0')
+            return false;
+    return true;
+}
+
+bool Validity::check_email(string email)
+{
+    int seperator_count = 0;
+    for (int i = 0; i < email.size(); i++)
+        if(email[i] == '@')
+            seperator_count++;
+    if(seperator_count != 1)
+        return false;
+    return true;
+}
