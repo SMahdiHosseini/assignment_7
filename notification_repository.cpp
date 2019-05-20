ade1fe796fab887b6c3f19aa8f89714480858ec4
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2019-05-20 16:45:37 +0430

    notification repository

---
+++ notification_repository.cpp
@@ -0,0 +1,35 @@
+
+#include "notification_repository.h"
+
+using namespace std;
+
+void NotificationRepository::give_notification(Notification notification)
+{
+    notifications.push_back(notification);
+}
+
+void NotificationRepository::show_unread_notifications()
+{
+    for (int i = notifications.size() - 1; i >= 0; i--)
+    {
+        if(!notifications[i].get_read())
+        {
+            cout << to_string(i) << ".";
+            notifications[i].read_massage();
+            notifications[i].show_massage();
+        }
+    }
+}
+
+void NotificationRepository::show_notifications(int limit)
+{
+    for (int i = notifications.size() - 1; i >= 0; i--)
+    {
+        if(notifications[i].get_read() && limit > 0)
+        {            
+            cout << to_string(i) << ".";
+            notifications[i].show_massage();
+            limit--;
+        }
+    }
+}
